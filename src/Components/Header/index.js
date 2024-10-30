d815a8b619945adbab5f2a6eeb7bd70689549201
Akshat Kumar Nautiyal <akshatnautiyal12@gmail.com>
2024-10-30 11:41:40 +0530

    home page and header

---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+const Header= ()=>{
+    return(
+        <>
+        <div className="header">
+            <div className="top-strip">
+                <div className="container">
+                    <p className="mb-0 mt-0 text-center">Due to the <b>COVID 19</b> epidemic, orders may be processed with a slight delay</p>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Header;
